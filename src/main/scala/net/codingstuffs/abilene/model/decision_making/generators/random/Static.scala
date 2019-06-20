9b801b587b7b17ab88c7f289aa9a0388f6cff271
IDoCodingStuffs <sahinvictor@gmail.com>
2019-06-20 17:49:08 -0400

    Proper implementation for static generator

--- src/main/scala/net/codingstuffs/abilene/model/decision_making/generators/random/Static.scala
+++ src/main/scala/net/codingstuffs/abilene/model/decision_making/generators/random/Static.scala
@@ -3,9 +3,9 @@ package net.codingstuffs.abilene.model.decision_making.generators.random
 import scala.util.Random
 
 object Static {
-  final val GENERATOR = new Static
+  final val GENERATOR = new Static(0, 0.25, 0.5, 0.75, 1)
 }
 
-class Static extends Random{
-  override def nextDouble: Double = 0.5
+class Static(values: Double*) extends Random{
+  override def nextDouble: Double = shuffle(values).head
 }
