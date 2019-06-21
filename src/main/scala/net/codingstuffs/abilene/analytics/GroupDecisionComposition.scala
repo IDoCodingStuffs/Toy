c31f792ec87db58fefbe892c45ae1d1edaf061c5
IDoCodingStuffs <sahinvictor@gmail.com>
2019-06-21 19:10:54 -0400

    Added analytics to count paradoxes

--- src/main/scala/net/codingstuffs/abilene/analytics/GroupDecisionComposition.scala
+++ src/main/scala/net/codingstuffs/abilene/analytics/GroupDecisionComposition.scala
@@ -16,4 +16,15 @@ class GroupDecisionComposition(df: DataFrame) {
 
   def getYesVoteCounts: DataFrame = processedDf.groupBy("acceptance").count
 
+  import df.sparkSession.implicits._
+  import org.apache.spark.sql.functions._
+
+  def preferencePerMember: DataFrame = df
+    .select("groupId", "selfPreference", "decision")
+    .withColumn("inclination", $"selfPreference" > 0.5)
+    .withColumn("paradox", $"decision" =!= $"inclination")
+
+  def decisionParadoxes: DataFrame = preferencePerMember
+    .groupBy("groupId")
+    .agg(sum($"paradox".cast(IntegerType)).alias("counts"))
 }
